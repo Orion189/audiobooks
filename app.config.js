2b3bee0b757118afdffda9736abbd61fb274718c
Yevhen Lepekha <orion189@gmail.com>
2024-06-20 11:01:36 +0300

    Update PlayStore URL

--- app.config.js
+++ app.config.js
@@ -34,7 +34,7 @@ module.exports = {
             playStoreUrl: process.env.GOOGLE_PLAY_LINK,
             softwareKeyboardLayoutMode: 'pan',
             googleServicesFile: process.env.GOOGLE_SERVICES_FILE_ANDROID,
-            versionCode: 1
+            versionCode: 2
         },
         web: {
             favicon: './assets/favicon.png'
